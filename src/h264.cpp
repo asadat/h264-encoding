d48f337553105347c2d3e01027c659548d15fcef
Abbas <abbas.sadat@gmail.com>
2014-02-12 18:01:54 -0800

    tweak

--- src/h264.cpp
+++ src/h264.cpp
@@ -2,9 +2,9 @@
 
 using namespace cv;
 
-int main(int, char**)
+int main(int argc, char** argv)
 {
-    VideoCapture cap(0); // open the default camera
+    VideoCapture cap(argv[1]); // open the default camera
     if(!cap.isOpened())  // check if we succeeded
         return -1;
 
@@ -14,10 +14,10 @@ int main(int, char**)
     {
         Mat frame;
         cap >> frame; // get a new frame from camera
-        cvtColor(frame, edges, CV_BGR2GRAY);
-        GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
-        Canny(edges, edges, 0, 30, 3);
-        imshow("edges", edges);
+//        cvtColor(frame, edges, CV_BGR2GRAY);
+//        GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
+//        Canny(edges, edges, 0, 30, 3);
+        imshow("edges", frame);
         if(waitKey(30) >= 0) break;
     }
     // the camera will be deinitialized automatically in VideoCapture destructor
